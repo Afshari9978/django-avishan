463a49fc7c8fe4893515d00eeb48872af5c0c726
Morteza Afshari <afshari9978@gmail.com>
2019-10-20 14:26:55 +0330

    0.2.0 -- move to project for tests

--- avishan/decorators.py
+++ avishan/decorators.py
@@ -1,11 +1,15 @@
 class AvishanView:
-    def __init__(self):
-        pass
+    def __init__(self, methods=None):
+        if methods is None:
+            methods = ['GET']
+        self.methods = methods
 
     def __call__(self, view_function):
-        # todo http method check
-        def wrapper(*args, **kwargs):
 
+        def wrapper(*args, **kwargs):
+            from . import current_request
+            if current_request['request'].method not in self.methods:
+                pass  # todo http method check
             try:
                 result = view_function(*args, **kwargs)
             except:
@@ -16,7 +20,18 @@ class AvishanView:
         return wrapper
 
 
+class AvishanApiView(AvishanView):
+    def __init__(self, methods=None):
+        self.type = 'api'
+        super().__init__(methods=methods)
+
+
+class AvishanTemplateView(AvishanView):
+    def __init__(self, methods=None):
+        self.type = 'template'
+        super().__init__(methods=methods)
+
+
 class AvishanCalculate:
     # todo: execution time
     pass
-
